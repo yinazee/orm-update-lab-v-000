d239f0449a4019bd8a5fab4c5a3f82640cf7171b
Yina Zhu <yinagit@gmail.com>
2018-04-22 03:07:30 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,4 +65,14 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    
+    student
+  end
+    
+    
+  end
+
 end

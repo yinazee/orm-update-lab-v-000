802bb5111637a30d358985f0a637b7ff0e225e4b
Yina Zhu <yinagit@gmail.com>
2018-04-22 02:54:16 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -45,9 +45,18 @@ class Student
   end
 
   def self.find_by_name(name)
-    sql = "SELECT * FROM students WHERE name = ?"
-    result = DB[:conn].execute(sql, name)[0]
-    Student.new(result[0], result[1], result[2])
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+    # The return value of the .map method is an array,
+    # and we're simply grabbing the .first element from the returned array. 
   end
 
   def update

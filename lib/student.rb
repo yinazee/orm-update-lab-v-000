eb7389c077ea85507be662e17951d097ac1fd25f
Yina Zhu <yinagit@gmail.com>
2018-04-22 03:30:23 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,8 +2,81 @@ require_relative "../config/environment.rb"
 
 class Student
 
+  attr_accessor :name, :grade
+  attr_reader :id
+
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
+  def initialize(id = nil, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students(
+        id Integer Primary key,
+        name TEXT,
+        grade Integer
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL
+
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+    # The return value of the .map method is an array,
+    # and we're simply grabbing the .first element from the returned array.
+  end
+
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+
+    student
+  end
+
+  def self.new_from_db(row)
+     id = row[0]
+     name = row[1]
+     grade = row[2]
+     self.new(id, name, grade)
+   end
 
 end

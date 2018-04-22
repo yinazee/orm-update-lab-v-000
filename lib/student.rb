2c68abfdf7d2d82cfe5838c4e412793f84464419
Yina Zhu <yinagit@gmail.com>
2018-04-22 02:37:39 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,7 +44,14 @@ class Student
   end
 end
 
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Student.new(result[0], result[1], result[2])
+  end
+end
+
   def self.update
   end
-  
+
 end
